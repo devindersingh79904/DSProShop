b5221c905023c55d67c7e2a84a04f421530cd0ec
Devinder Singh <devindersingh79904@gmail.com>
2020-12-28 20:39:02 +0530

    ready to work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,13 @@ function App() {
   return (
     <>
       <Header />
+
       <main className='py-3'>
         <Container>
           <h1>this is main div</h1>
         </Container>
       </main>
+
       <Footer />
     </>
   )
