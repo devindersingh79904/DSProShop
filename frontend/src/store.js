f0ac60f90e1a75e6d033da5d6896097ed83fccd6
Devinder Singh <devindersingh79904@gmail.com>
2021-01-10 21:29:03 +0530

    user authenticated

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,19 +7,25 @@ import {
 } from './reducers/productReducers'
 
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsSReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 })
 
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 }
 
 const middleware = [thunk]
